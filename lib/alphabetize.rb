5adb661d83a4f40a5dcc912454fba162bc5d8bb5
Paul Ashour <paul.ashour@gmail.com>
2018-10-10 22:58:39 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,16 @@
-def alphabetize(arr)
+def esperanto_sorter
   # code here
+  ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  alpha_hash = {}
+  ESPERANTO_ALPHABET.chars.each do |c|
+    alpha_hash[c] = ESPERANTO_ALPHABET.chars.index(c)
+  end
+  ['c','g','h','j','s','u'].each do |c|
+    alpha_hash["#{c}\u0302"] = alpha_hash[c] + 1
+  end
+end
+
+def alphabetize(array)
+  es = esperanto_sorter
+  array.sort_by { |w| w.chars.map { |c| es[c] } }
 end
