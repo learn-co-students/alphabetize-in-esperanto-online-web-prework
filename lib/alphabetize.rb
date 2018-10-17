cb50f1aed1447f23d793c4ffa2c0466e122399ed
Brandon Teich <brandonteich123@gmail.com>
2018-10-17 23:28:39 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,6 @@
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   # code here
-  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |x|
   x.split('').map do |y|
       return ESPERANTO_ALPHABET.index(y)
