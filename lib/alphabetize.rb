4e222543c32a75bb0a97cdfdb7db7e5eb9ca841b
Brandon Teich <brandonteich123@gmail.com>
2018-10-17 23:27:12 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,4 +2,6 @@ def alphabetize(arr)
   # code here
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |arrange|
+    arange.split('').map do |letter|
+      esperanto_alphabet.index(letter)
 end
