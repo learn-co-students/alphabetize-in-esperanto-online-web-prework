4192f75a10860c83c257af1c0c5d470d604c9103
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-21 00:38:37 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
 def alphabetize(arr)
   # code here
+  # my esperanto alphabet
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  # ascii values that are alphabetwise are same worth if they are in the same order 
+  ascii_equivalent = "`abcdefghijklmnopqrstuvwxyz{"
+
+  # Compare the array by changing its values to ones we can more easily compare 
+  # so change from esperanto to any ascii code in order and same length
+  # and then regular sort by comparing
+  arr.sort_by{|phrase1| phrase1.tr(esperanto_alphabet, ascii_equivalent)}
 end
