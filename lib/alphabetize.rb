4b9ef4b2286b511311fd9aac72d13fe9f3534430
VICTORIA MANUEL <victoriamanuel.dev@gmail.com>
2018-10-22 14:31:01 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
 def alphabetize(arr)
-  # code here
+   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  
+  arr.sort_by do |word|
+    word.split('').collect do |letter|
+       alphabet.index(letter)
+  
+   end
+  end
 end
