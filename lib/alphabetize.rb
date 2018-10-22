07a905f19855994b9a95f089d17bae5332c342d3
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-22 15:59:48 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+#binding.pry
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |phrase|
+    #binding.pry
+    new_array = phrase.split("")
+    #binding.pry
+    new_array.collect do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+    end
+  end
 end
