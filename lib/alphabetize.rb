8da39537f620b546f5383223df8e57952118fba3
Michael Soares <mikey.soares@gmail.com>
2018-12-14 17:15:16 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   # code here
+
+
+  arr.sort_by do |sentence|
+    sentence.split("").map do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+
+    end
+  end
 end
