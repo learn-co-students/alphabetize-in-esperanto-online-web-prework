950b01d3912303f3608857231c4c5828f712dd91
Henry Sztanski <henry@sztanski.com>
2018-12-19 13:43:05 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,15 @@
+require "pry"
+
+arr = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+
 def alphabetize(arr)
-  # code here
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+  arr.sort_by do |s|
+    s.split("").map do |char|
+      esperanto.index(char)
+    end
+  end
 end
+
+puts alphabetize(arr)
