344a18d49793a885044ad98d178c94b783f22dac
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-12 20:05:48 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
+
 def alphabetize(arr)
-  # code here
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |string|
+    string.chars.map do |c| 
+      esperanto.index(c)
+    end
+  end
 end
