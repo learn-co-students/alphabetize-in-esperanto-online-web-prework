db054c3df74045f67d6a34a6ae22a7a65403a831
Daniel Schneider <daschne8@gmail.com>
2019-03-07 19:21:06 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  return arr.sort_by {|string| string.chars.map{|c| alphabet.index(c)}}
 end
+
+arr = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+alph_map = alphabet.each_char.with_index.to_a.to_h
+new_arr = arr.sort_by {|string| string.chars.map{|c| alphabet.index(c)}}
+# binding.pry
+# puts "unbind"
