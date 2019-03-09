ebf8b0c722b2cf4f72e97121f798c9723c7f37b1
Cody Yantis <cyantis@gmail.com>
2019-03-09 18:44:44 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  e_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  binding.pry
+  sort_arr = arr.sort_by { |a| e_alphabet.index(a[0]) }
+  sort_arr.each do |a|
+  end
 end
