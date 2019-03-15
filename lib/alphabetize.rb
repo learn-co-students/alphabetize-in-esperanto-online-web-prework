0daf1e6ee448c1e4fca9c57051b5ba3e799bef93
blitzparanoia <bluemilk44@gmail.com>
2019-03-15 16:14:14 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
   # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |phrase|
+    phrase.chars.map do |character|
+      alphabet.index(character)
+    end
+  end
 end
