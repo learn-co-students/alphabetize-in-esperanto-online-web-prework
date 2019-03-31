b9a89bb39681efe98abef8c400e4c4151c384334
Natasha Chernis <natashachernis@gmail.com>
2019-03-31 05:09:03 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
-def alphabetize(arr)
-  # code here
+require 'pry'
+
+ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+def alphabetize(phrases_arr)
+  phrases_arr.sort_by do |phrase|
+    phrase.split("").map do |character|
+      ALPHABET.index(character)
+    end
+  end
 end
