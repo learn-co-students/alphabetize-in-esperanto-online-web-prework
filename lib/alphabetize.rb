630689b746cf18075b965d126ea25d77f4a6bc4d
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-05-04 22:44:42 +0800

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,18 +1,7 @@
 
 require "pry"
 
-class Array
-  def same_val?
-    self.uniq.length==1
-  end
-end
-
 def alphabetize(arr)
   esperanto= "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  first_letters=arr.map{|i| i[0]}
-  if first_letters.same_val?
-    arr.sort_by{|word| word}
-  else
-    arr.sort_by!{|word| esperanto.index(word[0])}
-  end
+  arr.sort_by { |i| i.split("").map{ |char| esperanto.index(char) } }
 end
