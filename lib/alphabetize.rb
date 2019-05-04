db9b13a60c013f060a934b5f743ec065e043e179
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-05-04 18:04:33 +0800

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,18 @@
+
+require "pry"
+
+class Array
+  def same_val?
+    self.uniq.length==1
+  end
+end
+
 def alphabetize(arr)
-  # code here
+  esperanto= "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  first_letters=arr.map{|i| i[0]}
+  if first_letters.same_val?
+    arr.sort_by{|word| word}
+  else
+    arr.sort_by!{|word| esperanto.index(word[0])}
+  end
 end
