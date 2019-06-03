1eb8c33fb0dc52f7eabf5f21505892dc40496b87
Angela Lo <angelawenlo@gmail.com>
2019-06-03 14:31:20 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
+require 'pry'
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |sentence|
+    sentence.split("").map do |character|
+      alphabet.index(character)
+    end
+  end
 end
